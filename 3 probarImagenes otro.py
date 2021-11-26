de83b9f54dbf6a92512692b7735bd1a5aa600401
Markos Ayala <elmarkos23@gmail.com>
2021-11-26 09:05:53 -0500

    COntador de imagenes

--- 3 probarImagenes otro.py
+++ 3 probarImagenes otro.py
@@ -1,18 +1,14 @@
 import cv2
 import os
 
-dataPath = 'imagenes' #Cambia a la ruta donde hayas almacenado Data
+dataPath = 'imagenes' #Carpeta donde se encuentra las imagenes
 numCamara = 0
 imagePaths = os.listdir(dataPath)
 print('imagePaths=',imagePaths)
 
-# 1 #face_recognizer = cv2.face.EigenFaceRecognizer_create()
-# 2 #face_recognizer = cv2.face.FisherFaceRecognizer_create()
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 # Leyendo el modelo
-#face_recognizer.read('modeloEigenFace.xml')
-#face_recognizer.read('modeloFisherFace.xml')
 face_recognizer.read('modeloLBPHFace.xml')
 
 cap = cv2.VideoCapture(numCamara,cv2.CAP_DSHOW)
@@ -28,21 +24,27 @@ while True:
 
 	faces = faceClassif.detectMultiScale(gray,1.3,5)
 
+	contador = 0
+
 	for (x,y,w,h) in faces:
 		rostro = auxFrame[y:y+h,x:x+w]
 		rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
 		result = face_recognizer.predict(rostro)
 
 		cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
-		
+
+		contador = contador + 1
+
+		cv2.putText(frame, "Total Personas " + str(contador),(20,50),2,1.1,(255,0,0),1,cv2.LINE_AA)
+
 		# LBPHFace
 		if result[1] < 70:
-			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+			cv2.putText(frame,'{}'.format(imagePaths[result[0]]) + " # " + str(contador),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
 			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
 		else:
-			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+			cv2.putText(frame,'Desconocido # ' + str(contador),(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
 			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-		
+
 	cv2.imshow('frame',frame)
 	k = cv2.waitKey(1)
 	if k == 27:
